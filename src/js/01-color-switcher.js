a092a7aac096df30f4036ee8db924e316f547ca0
VitalijO <onyshko.vitalij@gmail.com>
2022-10-10 02:06:30 +0300

    d
    d

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,39 @@
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body')
+
+let timerId = null;
+const DELAY = 1000;
+  
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function switchBodyColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor(); 
+}
+
+startBtn.addEventListener("mouseleave", () => {
+
+    if (startBtn.dataset.start>0) {
+    return
+    }
+
+    startBtn.setAttribute("class", "btn btn-secondary")
+
+  timerId = setInterval(() => {
+    
+        switchBodyColor() 
+        startBtn.dataset.start = 1
+  }, DELAY);
+}); 
+
+startBtn.addEventListener('click', switchBodyColor)
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.dataset.start = 0
+    startBtn.removeAttribute("class", "btn-secondary")
+    startBtn.setAttribute("class", "btn btn-primary ")
+});
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,86 @@
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const input = document.querySelector("input#datetime-picker")
+const startBtn = document.querySelector('button[data-start]')
+startBtn.setAttribute("class", "btn btn-primary")
+let daysVal = document.querySelector('.value[data-days]')
+let hoursVal = document.querySelector('.value[data-hours]')
+let minutesVal = document.querySelector('.value[data-minutes]')
+let secondsVal = document.querySelector('.value[data-seconds]')
+
+ 
+const DELAY = 1000;
+let isActive = false
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      if (selectedDates[0].getTime() < Date.now()){
+          window.alert("Please choose a date in the future") 
+      }
+    },
+};
+
+flatpickr(input, options)
+
+startBtn.addEventListener('click', start)
+
+function start() {
+const shoosedTime = Date.parse(input.value)
+    
+    if (isActive) {
+           return
+    }
+     startBtn.removeAttribute("class", "btn btn-primary")
+        startBtn.setAttribute("class", "btn btn-secondary")
+
+    timerId = setInterval(() => {
+           
+           isActive = true
+           const currentTime = Date.now() 
+           const deltaTime = shoosedTime - currentTime
+           if (deltaTime < 0) {
+               return
+           }
+       
+           const { days, hours, minutes, seconds } = convertMs(deltaTime) 
+           daysVal.textContent = `${days}`
+           hoursVal.textContent = `${hours}`
+           minutesVal.textContent = `${minutes}`
+           secondsVal.textContent = `${seconds}`
+           
+       }, DELAY)
+    
+    }
+ 
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad (Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad( Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function pad(val) {
+    return String(val).padStart(2, "0")
+}
+    
+ 
